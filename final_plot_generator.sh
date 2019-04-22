8975117fa9e55f9024c2f8c98e4eb100025d91c7
Miriyala Jeevan Kumar <jeevank@cse.iitk.ac.in>
2019-04-23 02:15:12 +0530

    wiki second time 5-iterations output for boxplot is added

--- final_plot_generator.sh
+++ final_plot_generator.sh
@@ -11,9 +11,10 @@ rm -rf avgPlot* 1>&2 2>/dev/null
 for np in `seq 8 8 64`
 do
 	avg=0
-	for i in `seq 1 10`
+	for i in `seq 1 5`
 	do
-		./run_tau_jeevan.sh $np ./datasets/as2000_nds_6474_edges_13233 13233 6474
+		echo "running_without_opt_np : "$np", iteration : "$i 
+		./run_tau_jeevan.sh $np datasets/wiki-talk_eds_5021410_nds_2394385 5021410 2394385
 		temp=$(pprof | tail -20 | head -1 | tr  -s " " | cut -d " " -f  3 | tr -d "," )
 		echo "size : "$np" , total_time : "$temp >> boxplot_without_opt
 		#avg=$(( avg+temp ))
@@ -34,9 +35,10 @@ echo "running with Optimizatoin .."
 for np in `seq 8 8 64`
 do
 	avg=0
-	for i in `seq 1 10`
+	for i in `seq 1 5`
 	do
-		./run_tau_jeevan.sh $np  ./datasets/as2000_nds_6474_edges_13233 13233 6474
+		echo "running_with_opt_np : "$np", iteration : "$i 
+		./run_tau_jeevan.sh $np  datasets/wiki-talk_eds_5021410_nds_2394385 5021410 2394385
 		temp=$(pprof | tail -20 | head -1 | tr  -s " " | cut -d " " -f  3 | tr -d "," )
 		echo "size : "$np" , total_time : "$temp >> boxplot_with_opt
 		#avg=$(( avg+temp ))
